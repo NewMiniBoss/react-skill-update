4b3ab61cdf9f035830660fc9825b7d79ea54c850
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-11-23 17:37:06 -0300

    primeiro commit

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,10 @@
+import './CampoTexto.css';
+const CampoTexto = () => {
+    return (
+        <form>
+            <label>Nome</label>
+            <input></input>
+        </form>
+    )
+}
+export default CampoTexto
