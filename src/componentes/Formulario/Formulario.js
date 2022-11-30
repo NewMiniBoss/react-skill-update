9a642edab2af05ae60e7a8f212228abdfea364cb
NewMiniBoss <gabriel.f.lazari@gmail.com>
2022-11-30 17:35:02 -0300

    puxando estilos dos componentes

---
+++ src/componentes/Formulario/Formulario.js
@@ -0,0 +1,36 @@
+import './Formulario.css'
+import CampoTexto from '../CampoTexto/index'
+import ListaSuspensa from '../ListaSuspensa/ListaSuspensa'
+import Botao from '../Botao/index'
+const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data-Science',
+        'Devops',
+        'Ux e Desing',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    }
+    return (
+        <section className='formulario'>
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times} />
+                <Botao>
+                    Criar Card
+                </Botao>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
